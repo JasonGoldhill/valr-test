86b4decfdde2dfc9ead39ffc08e9d4d875f089a2
Jason Goldhill <jasong@dotmodus.com>
2021-08-28 15:59:42 +0200

    added initial terraform for creating a gke cluster

---
+++ terraform/settings.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.52.0"
+    }
+  }
+
+  required_version = "~> 0.14"
+}
+
+provider "google" {
+  project = var.gcp_project_id
+  credentials = var.gcp_credentials
+  region = var.gcp_region
+  zone = var.gcp_zone
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,28 @@
+variable "gcp_credentials" {
+  type = string
+  sensitive = true
+  description = "Google Cloud service account credentials"
+}
+
+variable "gcp_project_id" {
+    type = string
+    sensitive = false
+    description = "Google Cloud project ID"
+}
+
+variable "gcp_region" {
+    type = string
+    sensitive = false
+    description = "Google Cloud region"
+}
+
+variable "gcp_zone" {
+    type = string
+    sensitive = false
+    description = "Google Cloud zone"
+}
+
+variable "gke_num_nodes" {
+  default     = 2
+  description = "Number of gke nodes"
+}
