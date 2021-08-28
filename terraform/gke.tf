6046e6cd6820ff89d24c250d5817f81649ff6326
Jason Goldhill <jasong@dotmodus.com>
2021-08-28 16:20:00 +0200

    terraform fmt

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -1,7 +1,7 @@
 resource "google_container_cluster" "primary" {
   name     = "${var.gcp_project_id}-gke"
   location = var.region
-  
+
   remove_default_node_pool = true
   initial_node_count       = 1
 
