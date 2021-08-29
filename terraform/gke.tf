9f17363692b0e58f88ab21cfd90eafbec733daf5
Jason Goldhill <jasong@dotmodus.com>
2021-08-29 08:25:55 +0200

    remove vpc and use default node pool

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -1,34 +1,22 @@
 resource "google_container_cluster" "primary" {
-  name     = "${var.gcp_project_id}-gke"
-  location = var.gcp_region
+  name               = "${var.gcp_project_id}-gke"
+  location           = var.gcp_region
+  initial_node_count = var.gke_num_nodes
 
-  remove_default_node_pool = true
-  initial_node_count       = 1
-
-  network    = google_compute_network.vpc.name
-  subnetwork = google_compute_subnetwork.subnet.name
-}
-
-resource "google_container_node_pool" "primary_nodes" {
-  name       = "${google_container_cluster.primary.name}-node-pool"
-  location   = var.gcp_region
-  cluster    = google_container_cluster.primary.name
-  node_count = var.gke_num_nodes
-
+  # network    = google_compute_network.vpc.name
+  # subnetwork = google_compute_subnetwork.subnet.name
   node_config {
     oauth_scopes = [
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/cloud-platform"
     ]
-
+    machine_type = "n1-standard-1"
     labels = {
       env = var.gcp_project_id
     }
-
-    machine_type = "n1-standard-1"
-    tags         = ["gke-node", "${var.gcp_project_id}-gke"]
-    metadata = {
-      disable-legacy-endpoints = "true"
-    }
+    tags = ["gke-node", "${var.gcp_project_id}-gke"]
+  }
+  timeouts {
+    create = "30m"
+    update = "40m"
   }
 }
