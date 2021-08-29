e1c3e93cfd5c9804f9446331b3b955816d18e2a1
Jason Goldhill <jasong@dotmodus.com>
2021-08-29 10:30:08 +0200

    change k8s provider to reference right cluster

--- terraform/settings.tf
+++ terraform/settings.tf
@@ -27,15 +27,11 @@ provider "google" {
 }
 
 data "google_client_config" "default" {}
-data "google_container_cluster" "my_cluster" {
-  name = "${var.gcp_project_id}-gke"
-  zone = var.gcp_region
-}
 
 provider "kubernetes" {
-  host                   = "https://${data.google_container_cluster.my_cluster.endpoint}"
+  host                   = "https://${data.google_container_cluster.primary.endpoint}"
   token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate)
+  cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
   experiments {
     manifest_resource = true
   }
