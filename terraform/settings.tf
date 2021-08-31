26e017a968a65121cd30ca437167973ec65a2689
Jason Goldhill <jasong@dotmodus.com>
2021-08-31 16:48:16 +0200

    enable k8s manifests

--- terraform/settings.tf
+++ terraform/settings.tf
@@ -39,4 +39,7 @@ provider "kubernetes" {
   host                   = "https://${data.google_container_cluster.my_cluster.endpoint}"
   token                  = data.google_client_config.default.access_token
   cluster_ca_certificate = base64decode(data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate)
+  experiments {
+    manifest_resource = true
+  }
 }
