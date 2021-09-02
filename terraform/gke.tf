77bb1445817cd26fa7f6b1ea762222a1e4bce8b0
Jason Goldhill <jasong@dotmodus.com>
2021-09-02 16:13:25 +0200

    remove ip allocation

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -3,8 +3,6 @@ resource "google_container_cluster" "primary" {
   location           = var.gcp_region
   initial_node_count = var.gke_num_nodes
 
-  # network    = google_compute_network.vpc.name
-  # subnetwork = google_compute_subnetwork.subnet.name
   node_config {
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
@@ -18,11 +16,5 @@ resource "google_container_cluster" "primary" {
   timeouts {
     create = "30m"
     update = "40m"
-  }
-  ip_allocation_policy {
-    cluster_ipv4_cidr_block       = "10.92.0.0/14"
-    cluster_secondary_range_name  = "gke-${var.gcp_project_id}-gke-pods-09f3092d"
-    services_ipv4_cidr_block      = "10.96.0.0/20"
-    services_secondary_range_name = "gke-${var.gcp_project_id}-gke-services-09f3092d"
   }
 }
